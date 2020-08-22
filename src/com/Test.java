211c3a5f3672f5a54e20ddb913026bac0f63d4cf
SPOTO-TianMa <1684018881@qq.com>
2020-08-22 21:34:40 +0800

    second

--- src/com/Test.java
+++ src/com/Test.java
@@ -2,6 +2,7 @@ package com;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println("master0");
         System.out.println("master1");
         System.out.println("dev01");
     }
